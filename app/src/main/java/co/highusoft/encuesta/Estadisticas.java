51fd1911d77618fdc5aa7fb2b6e53f01509f8b10
leonleo997 <yesid.lopez@correo.icesi.edu.co>
2018-11-08 17:26:06 -0500

    change to percentages

--- app/src/main/java/co/highusoft/encuesta/Estadisticas.java
+++ app/src/main/java/co/highusoft/encuesta/Estadisticas.java
@@ -32,20 +32,21 @@ public class Estadisticas extends AppCompatActivity {
     private PieChart pieChart_p5;
     FirebaseAuth auth;
     FirebaseDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas);
 
 
-        pieChart_p1=findViewById(R.id.pie_chart_p1);
-        pieChart_p2=findViewById(R.id.pie_chart_p2);
-        pieChart_p3=findViewById(R.id.pie_chart_p3);
-        pieChart_p4=findViewById(R.id.pie_chart_p4);
-        pieChart_p5=findViewById(R.id.pie_chart_p5);
+        pieChart_p1 = findViewById(R.id.pie_chart_p1);
+        pieChart_p2 = findViewById(R.id.pie_chart_p2);
+        pieChart_p3 = findViewById(R.id.pie_chart_p3);
+        pieChart_p4 = findViewById(R.id.pie_chart_p4);
+        pieChart_p5 = findViewById(R.id.pie_chart_p5);
 
-        db=FirebaseDatabase.getInstance();
-        auth=FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
 
         loadPieChart_p1();
     }
@@ -53,35 +54,35 @@ public class Estadisticas extends AppCompatActivity {
     private void loadPieChart_p1() {
 
 
-        Description desc= new Description();
+        Description desc = new Description();
         desc.setText("Pregunta 1");
         desc.setTextSize(15);
         pieChart_p1.setDescription(desc);
         pieChart_p1.setRotationEnabled(true);
 
 
-        Description desc2= new Description();
+        Description desc2 = new Description();
         desc2.setText("Pregunta 2");
         desc2.setTextSize(15);
         pieChart_p2.setDescription(desc2);
         pieChart_p2.setRotationEnabled(true);
 
 
-        Description desc3= new Description();
+        Description desc3 = new Description();
         desc3.setText("Pregunta 3");
         desc3.setTextSize(15);
         pieChart_p3.setDescription(desc3);
         pieChart_p3.setRotationEnabled(true);
 
 
-        Description desc4= new Description();
+        Description desc4 = new Description();
         desc4.setText("Pregunta 4");
         desc4.setTextSize(15);
         pieChart_p4.setDescription(desc4);
         pieChart_p4.setRotationEnabled(true);
 
 
-        Description desc5= new Description();
+        Description desc5 = new Description();
         desc5.setText("Pregunta 5");
         desc5.setTextSize(15);
         pieChart_p5.setDescription(desc5);
@@ -92,9 +93,6 @@ public class Estadisticas extends AppCompatActivity {
         addDataSet(pieChart_p3, "Pregunta_3");
         addDataSet(pieChart_p4, "Pregunta_4");
         addDataSet(pieChart_p5, "Pregunta_5");
-
-
-
 
 
         Log.e(">>P1", "loadPieChart_p1: ");
@@ -113,27 +111,38 @@ public class Estadisticas extends AppCompatActivity {
                 xData.clear();
                 Iterable<DataSnapshot> children = dataSnapshot.getChildren();
 
-                for (DataSnapshot child: children) {
+                for (DataSnapshot child : children) {
                     yData.add(new Float(child.getChildrenCount()));
                     xData.add(child.getKey());
 
-                    Log.e(">>p1", "onDataChange: "+child.getKey()+" values:"+child.getChildrenCount() );
-                }
-                ArrayList<PieEntry> yEntries= new ArrayList<>();
-                ArrayList<String> xEntries= new ArrayList<>();
-                for (int i=0; i<yData.size(); i++){
-                    yEntries.add(new PieEntry(yData.get(i),xData.get(i)));
+                    Log.e(">>p1", "onDataChange: " + child.getKey() + " values:" + child.getChildrenCount());
                 }
 
-                for (int i=0; i<xData.size(); i++){
+                Float total = 0f;
+                for (Float value :
+                        yData) {
+                    total += value;
+                }
+
+                for (int i = 0; i < yData.size(); i++) {
+                    yData.set(i, (yData.get(i)*100) / total);
+                }
+
+                ArrayList<PieEntry> yEntries = new ArrayList<>();
+                ArrayList<String> xEntries = new ArrayList<>();
+                for (int i = 0; i < yData.size(); i++) {
+                    yEntries.add(new PieEntry(yData.get(i), xData.get(i)));
+                }
+
+                for (int i = 0; i < xData.size(); i++) {
                     xEntries.add(xData.get(i));
                 }
 
-                PieDataSet pieDataSet= new PieDataSet(yEntries, "Answers");
+                PieDataSet pieDataSet = new PieDataSet(yEntries, "Answers");
                 pieDataSet.setSliceSpace(2);
                 pieDataSet.setValueTextSize(12);
 
-                ArrayList<Integer> colors= new ArrayList<>();
+                ArrayList<Integer> colors = new ArrayList<>();
                 colors.add(Color.GRAY);
                 colors.add(Color.BLUE);
                 colors.add(Color.GREEN);
@@ -147,7 +156,7 @@ public class Estadisticas extends AppCompatActivity {
                 legend.setForm(Legend.LegendForm.CIRCLE);
                 legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
 
-                PieData pieData= new PieData(pieDataSet);
+                PieData pieData = new PieData(pieDataSet);
                 pieChart_p1.setData(pieData);
                 pieChart_p1.invalidate();
             }
